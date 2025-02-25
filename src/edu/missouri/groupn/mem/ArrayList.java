8d92b15a5832e34cc0c1111e58717c62f25187bf
creallfluharty <creallfluharty@gmail.com>
2025-02-24 21:49:24 -0600

    add untested arraylist implementation

---
+++ src/edu/missouri/groupn/mem/ArrayList.java
@@ -0,0 +1,46 @@
+package edu.missouri.groupn.mem;
+
+public class ArrayList {
+	private Object[] array;
+	private int size;
+	
+	public ArrayList() {
+		this(1);
+	}
+	
+	public ArrayList(int initialCapacity) {
+		this.array = new Object[initialCapacity];
+		this.size = 0;
+	}
+	
+	private void doubleCapacity() {
+		var newArray = new Object[this.array.length * 2];
+		for (int i = 0; i < this.size; ++i) {
+			newArray[i] = this.array[i];
+		}
+		this.array = newArray;
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public Object at(int index) {
+		return this.array[index]; // throw the usual errors
+	}
+	
+	public void push(Object item) {
+		if (this.size == this.array.length) {
+			this.doubleCapacity();
+		}
+		
+		this.array[this.size++] = item;
+	}
+	
+	public void pop(int index) {
+		for (int i = index; i < this.size - 1; ++i) {
+			this.array[i] = this.array[i+1];
+		}
+		this.array[this.size--] = null;
+	}
+}
