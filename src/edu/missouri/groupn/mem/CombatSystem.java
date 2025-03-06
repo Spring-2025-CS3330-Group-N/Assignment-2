129ab2db87a4ce6eec2962d39f7f00530d1fe2d6
creallfluharty <creallfluharty@gmail.com>
2025-03-05 23:44:03 -0600

    docstrings for combat + prompter

--- src/edu/missouri/groupn/mem/CombatSystem.java
+++ src/edu/missouri/groupn/mem/CombatSystem.java
@@ -6,11 +6,18 @@ public class CombatSystem {
 
 	private static CombatSystem instance;
 
+	/**
+	 * create a combat system object
+	 */
 	private CombatSystem() {
 		this.damageRules = new ArrayList();
 		this.classes = new ArrayList();
 	}
 
+	/**
+	 * getInstance()
+	 * @return the instance of the combat system if it exists
+	 */
 	public static CombatSystem getInstance() {
 		if (instance == null) {
 			instance = new CombatSystem();
@@ -18,14 +25,33 @@ public class CombatSystem {
 		return instance;
 	}
 
+	/**
+	 * addClass():
+	 * @param c a class to add to the combat system. Note that the order
+	 * in which classes are added to the combat system determines
+	 * the effectiveness of attacks!
+	 */
 	public void addClass(Class c) {
 		this.classes.push(c);
 	}
 
+	/**
+	 * addDamageRule()
+	 * @param stride for any class at index i, that class's attacks are boosted
+	 * by multiplier against the class at i + stride. This is the q parameter of
+	 * https://en.wikipedia.org/wiki/Regular_polygon#Regular_star_polygons
+	 * @param multiplier the amount to boost attack damage against affected classes by
+	 */
 	public void addDamageRule(int stride, double multiplier) {
 		this.damageRules.push(new Object[]{stride, multiplier});
 	}
 
+	/**
+	 * getDamageMultiplier()
+	 * @param a the class of the character initiating the attack
+	 * @param b the class of the character receiving the attack
+	 * @return the effectiveness of the attack, as a multiplier of a's power
+	 */
 	public double getDamageMultiplier(Class a, Class b) {
 		int aCombatId = this.classes.find(a);
 		int bCombatId = this.classes.find(b);
--- src/edu/missouri/groupn/mem/ui/Prompter.java
+++ src/edu/missouri/groupn/mem/ui/Prompter.java
@@ -7,11 +7,21 @@ public class Prompter {
 	Validator validator;
 	Object invalid;
 
+	/**
+	 * create a prompter
+	 * @param prompt a string to display to the user when asking for input
+	 * @param validator a strategy for parsing input and determining whether it is valid
+	 */
 	public Prompter(String prompt, Validator validator) {
 		this.prompt = prompt;
 		this.validator = validator;
 	}
-	
+
+	/**
+	 * prompt():
+	 * ask the user for input and only return once a valid value has been entered
+	 * @return a parsed and validated input casted to the Object base class
+	 */
 	public Object prompt() {
 		var scanner = new Scanner(System.in);
 		
