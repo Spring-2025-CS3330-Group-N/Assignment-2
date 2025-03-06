63214bba4b5fcfa3f9c77ae8faec20e30bff94b8
Alyssa Norrenberns <alyssa.norrenberns@gmail.com>
2025-03-05 19:26:10 -0600

    updated CharacterManager to utilize custom ArrayList class
    starting adding javadocs to ArrayList methods

--- src/edu/missouri/groupn/mem/CharacterManager.java
+++ src/edu/missouri/groupn/mem/CharacterManager.java
@@ -3,8 +3,10 @@ package edu.missouri.groupn.mem;
 import edu.missouri.groupn.mem.characters.MiddleEarthCharacter;
 
 public class CharacterManager {
-	public MiddleEarthCharacter[] characters = new MiddleEarthCharacter[5];
-	public int size = 0;
+	// creates a list of characters with an initial capacity of 5
+	protected ArrayList characters = new ArrayList(5);
+	protected int size = 0;
+	// size variable is also included in our custom ArrayList class!
 	
 	/**
 	 * addCharacter():
@@ -13,14 +15,11 @@ public class CharacterManager {
 	 * @return True if addition is successful, otherwise return False
 	 */
 	public boolean addCharacter(MiddleEarthCharacter c) {
-		// check if array is full
-		if (this.characters.length == this.size) {
-			// logic for doubling the array -- create and call a separate method created to handle that?
-			// just return false for now, will add dynamic resize feature later
-			System.out.println("The system is full -- try again later.");
-			return false;
-		}
-		this.characters[this.size] = c;
+		// check for duplicate entry?
+		
+		// push() method includes conditions for dynamic array resizing
+		this.characters.push(c); 
+		
 		this.size++;
 		System.out.println("Success! " + c + " added to the system.");
 		return true;
@@ -35,10 +34,14 @@ public class CharacterManager {
 	public MiddleEarthCharacter getCharacter(String name) {
 		System.out.println("\nSearching for " + name + "...");
 		
-		for (int i=0; i<this.size; i++) {
-			if (this.characters[i].getName().equalsIgnoreCase(name)) {
+		// change how we get size here?
+		for (int i=0; i<this.characters.getSize(); i++) {
+			// must cast MiddleEarthCharacter type here to use getter methods from that class
+			var character = (MiddleEarthCharacter) this.characters.at(i);
+			
+			if (character.getName().equals(name)) {
 				System.out.println("Found " + name + " in the system!");
-				return this.characters[i];
+				return character;
 			}
 		}
 		System.out.print("No characters named " + name + " were found in the system.\n");
@@ -56,40 +59,46 @@ public class CharacterManager {
 	 */
 	public boolean updateCharacter(MiddleEarthCharacter character, String name, int health, int power) {
 		// search character management system for the given character
-		for (int i=0; i<this.size; i++) {
-			if (this.characters[i].equals(character)) {
-				// variable to compare argument values to those already stored for the character
-				// if unique, update character with the new value
-				var uniqueVal = false; 
-				
-				// check for unique name
-				if (!(this.characters[i].getName().equalsIgnoreCase(name))) {
-					uniqueVal = true;
-					this.characters[i].setName(name);
-				}
-				// check for unique health level
-				if (this.characters[i].getHealth() != health) {
-					uniqueVal = true;
-					this.characters[i].setHealth(health);
-				}
-				// check for unique power level
-				if (this.characters[i].getPower() != power) {
-					uniqueVal = true;
-					this.characters[i].setPower(power);
-				}
-				
-				// check uniqueVal
-				if (uniqueVal == true) {
-					System.out.println("Update successful! See new character stats below.");
-					this.characters[i].displayInfo();
-					System.out.println("\n");
-					return true;
-				}
-				else {
-					System.out.println("There is nothing to change or update for " + this.characters[i].getName());
-					System.out.println("Try again with different arguments for name, health, or power.\n");
-					return false;
-				}
+		// find() returns the index
+		var charIndex = this.characters.find(character);
+		
+		// check for valid index (AKA character exists in the array)
+		if (charIndex != -1) {
+			// must cast MiddleEarthCharacter type here to use getter/setter methods from that class
+			var existingCharacter = (MiddleEarthCharacter) this.characters.at(charIndex);
+			
+			// variable to compare argument values to those already stored for the character
+			// if unique, update character with the new value
+			var uniqueVal = false; 
+			
+			// check for unique name
+			if (! existingCharacter.getName().equals(name)) {
+				uniqueVal = true;
+				existingCharacter.setName(name);
+//				((MiddleEarthCharacter) this.characters.at(charIndex)).setName(name);
+			}
+			// check for unique health level
+			if (existingCharacter.getHealth() != health) {
+				uniqueVal = true;
+				existingCharacter.setHealth(health);
+			}
+			// check for unique power level
+			if (existingCharacter.getPower() != power) {
+				uniqueVal = true;
+				existingCharacter.setPower(power);
+			}
+			
+			// check uniqueVal
+			if (uniqueVal == true) {
+				System.out.println("Update successful! See new character stats below.");
+				existingCharacter.displayInfo();
+				System.out.println("\n");
+				return true;
+			}
+			else {
+				System.out.println("There is nothing to change or update for " + existingCharacter.getName());
+				System.out.println("Try again with different arguments for name, health, or power.\n");
+				return false;
 			}
 		}
 		System.out.print(character + " was not found. You must add characters to the system before you can update them.\n");
@@ -105,17 +114,22 @@ public class CharacterManager {
 			System.out.println("\nThe character management system is empty! Add character(s) and try again.\n");
 			return;
 		}
-		
+		 
 		// adjust grammar based on how many characters are in the system
 		if (this.size == 1) {
 			System.out.print("\nThere is currently " + this.size + " character in the system.\n");
+			// must cast MiddleEarthCharacter type here to use methods from that class
+			var character = (MiddleEarthCharacter) this.characters.at(size-1);
+			character.displayInfo();
 		}
 		else {
 			System.out.println("\nThere are currently " + this.size + " characters in the system:\n");
+			// print character(s)
+			for (int i=0; i<this.characters.getSize(); i++) {
+				// must cast MiddleEarthCharacter type here to use methods from that class
+				var character = (MiddleEarthCharacter) this.characters.at(i);
+				character.displayInfo();
 			}
-		// print character(s)
-		for (int i=0; i<this.size; i++) {
-			this.characters[i].displayInfo();
 		}
 	}
 }
