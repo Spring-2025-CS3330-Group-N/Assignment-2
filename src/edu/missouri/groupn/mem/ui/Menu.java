599f82b498c8557f89ab748baad7de23cc13fd4d
Alyssa Norrenberns <alyssa.norrenberns@gmail.com>
2025-03-05 23:49:47 -0600

    added javadocs for Menu

--- src/edu/missouri/groupn/mem/ui/Menu.java
+++ src/edu/missouri/groupn/mem/ui/Menu.java
@@ -2,19 +2,38 @@ package edu.missouri.groupn.mem.ui;
 
 import edu.missouri.groupn.mem.ArrayList;
 
+/**
+ * This class represents an interactive menu.
+ * It includes a prompt and list of menu options to choose from.
+ */
 public class Menu {
 	private String prompt;
 	private ArrayList options;
 
+	/**
+	 * Parameterized constructor:
+	 * This method creates an instance of Menu with a given prompt.
+	 * @param prompt Prompt for menu
+	 */
 	public Menu(String prompt) {
 		this.prompt = prompt;
 		this.options = new ArrayList();
 	}
 	
+	/**
+	 * This method adds a new option to options array list.
+	 * @param name Name of option
+	 * @param key Key associated with the option
+	 */
 	public void addOption(String name, Object key) {
 		this.options.push(new Object[]{name, key});
 	}
 	
+	/**
+	 * This method displays the prompt for the menu and all available options.
+	 * Prompts user input for choosing a menu option.
+	 * @return Key for the chosen menu option upon success, otherwise returns invalid
+	 */
 	public Object prompt() {
 		System.out.println(this.prompt);
 		for (var i = 0; i < options.getSize(); ++i) {
